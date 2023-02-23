5a7248c739f5e23e256395a4f085a57697d36d27
brennohb <breno_henriquebarbosa@hotmail.com>
2023-02-23 17:16:11 -0300

    ajuste de açao

--- salvar-usuario.php
+++ salvar-usuario.php
@@ -1,21 +1,18 @@
 <?php 
-switch($_REQUEST["acao"]) {
-case 'cadastrar':
-  $nome = $_POST["nome"];
-  $email = $_POST["email"];
-  $senha = $_POST["senha"];
-  $data_nasc = $_POST["data_nasc"];
-
-  $sql = "INSERT INTO usuario (nome,email,senha,data_nasc) VALUES ('{$nome}','{$email}', '{$senha}','{$data_nasc}');
-  $res = $conn -> query($sql);
-  break;
+switch ($_REQUEST["acao"]) {
+  case 'cadastar':
+    $nome = $_POST["nome"];
+    $email = $_POST["email"];
+    $senha = $_POST["senha"];
+    $data_nasc = $_POST["data_nasc"];
+    
+    $sql = "INSERT INTO usuarios (nome,email,senha,data_nasc) VALUES ('{$nome}','{$email}','{$senha}','{$data_nasc}')";
+    
+    $res = $conn -> query($sql);
+   break;
   
-  case 'editar':
-  
-  break;
-
-  case 'excluir':
-  break;
-} 
-
+  default:
+    # code...
+    break;
+}
 ?>
