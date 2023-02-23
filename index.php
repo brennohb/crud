bff93819deffbe6169cf439fd96c20f1468c28a8
brennohb <breno_henriquebarbosa@hotmail.com>
2023-02-23 11:41:18 -0300

    primeiras alteraçoes

---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <title>Cadastro</title>
+</head>
+
+<body>
+  <h1>Hello, world!</h1>
+  <script src="js/bootstrap.bundle.min.js">
+  </script>
+</body>
+
+</html>
