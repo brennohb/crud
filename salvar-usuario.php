2a7c7569847aa7fb48927f468d9c45a15058e627
brennohb <breno_henriquebarbosa@hotmail.com>
2023-02-23 16:38:29 -0300

    esboço de açoes

---
+++ salvar-usuario.php
@@ -0,0 +1,10 @@
+<?php 
+switch($_REQUEST["acao"]){
+  case 'cadastrar':
+  $nome = $_REQUEST["nome"];
+  $email = $_REQUEST["email"];
+  $senha = $_REQUEST["senha"];
+  $gata_nas = $_REQUEST["data"];
+  break;
+}
+?>
