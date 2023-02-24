644b645858a7d338115d42796b428a7a44e8e332
brennohb <breno_henriquebarbosa@hotmail.com>
2023-02-24 17:30:16 -0300

    ajuste

--- listar-usuario.php
+++ listar-usuario.php
@@ -23,6 +23,7 @@ if($qtd>0){
     <button onclick=\"location.href=?page=editar&id=".$row->id."';\" class='btn btn-success'>Editar</button>
     
     <button onclick=\"if(confirm('Tem certeza que deseja excluir?')){location.href=?page=salvar&acao=excluir&id=".$row->id."';}else{false;}\"  class='btn btn-danger'>Excluir</button>
+    
     </td>";
     print "</tr>";
   }
