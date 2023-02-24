0259f5cbd26067875bc4f76e534826d5e2a1b95a
brennohb <breno_henriquebarbosa@hotmail.com>
2023-02-24 18:36:35 -0300

    refazendo botoes

--- listar-usuario.php
+++ listar-usuario.php
@@ -22,7 +22,7 @@ if($qtd>0){
     print "<td>
     <button onclick=\"location.href='?page=editar&id=".$row->id."';\" class='btn btn-success'>Editar</button>
     
-    <button class='btn btn-danger'>Excluir</button>
+    <button onclick=\"if(confirm('Tem certeza que deseja excluir?')){location.href='?page=salvar&acao=excluir&id=".$row->id."';}else{false;}\" class='btn btn-danger'>Excluir</button>
           </td>";
     print "</tr>";
   }
